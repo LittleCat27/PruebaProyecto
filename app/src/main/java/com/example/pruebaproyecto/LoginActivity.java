5499c560b29eb729e3e9e94f4b2dd4d265199eed
LittleCat27 <gregoirejoaquin@gmail.com>
2023-05-26 20:21:06 -0300

    Agrego un timer luego de 3 intentos de contraseña fallidos.

--- app/src/main/java/com/example/pruebaproyecto/LoginActivity.java
+++ app/src/main/java/com/example/pruebaproyecto/LoginActivity.java
@@ -9,11 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.time.LocalTime;
+import java.time.temporal.TemporalAmount;
+import java.util.Calendar;
+import java.util.Date;
+
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class LoginActivity extends AppCompatActivity {
 
     byte intentos = 0;
+    Date tiempo = Calendar.getInstance().getTime();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +33,13 @@ public class LoginActivity extends AppCompatActivity {
         
         if (intentos >= 3)
         {
-            Toast.makeText(this, "Demasiados Intentos, espere segundos", Toast.LENGTH_SHORT).show();
+            long aux = tiempo.getTime() - Calendar.getInstance().getTimeInMillis();
+            long segundos = aux/1000;
+            Toast.makeText(this, "Demasiados Intentos, espere "+ segundos +" segundos", Toast.LENGTH_SHORT).show();
+
+            if(tiempo.getTime() <= Calendar.getInstance().getTimeInMillis())
             intentos = 0;
+            else return;
         }
         
         if (password.equals("1234") && username.equalsIgnoreCase("alumno")) {
@@ -42,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.LoginFail, Toast.LENGTH_SHORT).show();
             if (intentos >= 3)
             {
-
+                tiempo.setTime(Calendar.getInstance().getTimeInMillis() + 10000);
             }
         }
     }
