ef9ba878f812829c9ce3450650f28505587d72f1
LittleCat27 <gregoirejoaquin@gmail.com>
2023-05-26 14:28:09 -0300

    Saco el menu porque no sirve para un carajo.

--- app/src/main/java/com/example/pruebaproyecto/MainActivity.java
+++ app/src/main/java/com/example/pruebaproyecto/MainActivity.java
@@ -1,65 +1,23 @@
 package com.example.pruebaproyecto;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ClipData;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class MainActivity extends AppCompatActivity{
-
-    public DrawerLayout drawerLayout;
-    public ActionBarDrawerToggle actionBarDrawerToggle;
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FuncionMenu(); //Solo segui un tutorial, no lo hice mano :v
-    }
-
-    public void FuncionMenu()
-    {
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
     }
 
 
-    private void handleLogout() {
-
-        Intent intent = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            intent = new Intent(MainActivity.this, LoginActivity.class);
-        }
-        startActivity(intent);
-        finish();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            int id = item.getItemId();
-            if (id == R.id.i_logout){
-                handleLogout();
-                finish();
-            }
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
